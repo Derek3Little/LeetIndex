4cbd13e448b9f42e899d435e98e4e4fe779a4816
Derek E Little <derek3little@gmail.com>
2023-02-03 08:55:21 -0500

    Adding solution to problem.

--- KeepMultiplyingFoundValuesByTwo/src/LeetIndexTemplate/Problem.java
+++ KeepMultiplyingFoundValuesByTwo/src/LeetIndexTemplate/Problem.java
@@ -1,11 +1,31 @@
 package LeetIndexTemplate;
 
+import java.util.HashSet;
+
 public class Problem {
 
     // url of problem
+    // https://leetcode.com/problems/keep-multiplying-found-values-by-two/
 
+    // submitted? date?
 
-    // problem method and helper methods
+    public static int findFinalValue(int[] nums, int original) {
 
-    
+        // Set for collecting values since duplicates do not matter
+        // Set.contains() method has O(1) run time, so checking against set contents is fast
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        // add all values of array to set
+        for (int i = 0; i < nums.length; i++) {
+            hashSet.add(nums[i]);
+        }
+
+        // continue multiplying original by 2 as long as set contains original
+        while (hashSet.contains(original)) {
+            original *= 2;
+        }
+
+        // while loop exits when multiplied original cannot be found, return it!
+        return original;
+    }
 }
