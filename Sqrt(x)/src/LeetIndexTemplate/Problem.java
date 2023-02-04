4fbba3dc3f188d025962d317cf6cff6492acc9d3
Derek E Little <derek3little@gmail.com>
2023-02-04 08:55:00 -0500

    Adding solution to Sqrt(x) problem.

--- Sqrt(x)/src/LeetIndexTemplate/Problem.java
+++ Sqrt(x)/src/LeetIndexTemplate/Problem.java
@@ -7,8 +7,29 @@ public class Problem {
 
     // submitted? date?
 
+    public static int mySqrt(int x) {
 
-    // problem method and helper methods
+        // handling edge cases
+        if (x == 0 || x == 1) {
+            return x;
+        }
 
+        long lo = 1; // the minimum of the search space, long to account for values too large for int
+        long hi = x; // the maximum of the search space
 
+        while (lo < hi) { // binary search season!
+
+            long mid = lo + (hi - lo) / 2; // find mid
+
+            if (mid * mid > x) { // if greater, new hi is mid
+                hi = mid;
+            } else {
+                lo = mid + 1; // if lesser, new low is mid + 1
+            }
+        } // loop exits when lo crosses hi
+
+        return (int) lo - 1; // return must be converted from long to int
+
+        // standard issue binary search: O(logN)
+    }
 }
