da8626a5f24f687e3ebcf32012020742fce62392
Derek E Little <derek3little@gmail.com>
2023-02-04 08:43:10 -0500

    Initial commit for Sqrt(x), adding starter project files and template package.

---
+++ Sqrt(x)/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,14 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem
+    // https://leetcode.com/problems/sqrtx/
+
+    // submitted? date?
+
+
+    // problem method and helper methods
+
+
+}
---
+++ Sqrt(x)/src/LeetIndexTemplate/ProblemTest.java
@@ -0,0 +1,7 @@
+package LeetIndexTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProblemTest {
+
+}
