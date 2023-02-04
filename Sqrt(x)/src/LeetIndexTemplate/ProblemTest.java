2cf72b6e4fae0024d589257c4f15b74aa981117c
Derek E Little <derek3little@gmail.com>
2023-02-04 10:50:54 -0500

    Finishing test suite, all pass.

--- Sqrt(x)/src/LeetIndexTemplate/ProblemTest.java
+++ Sqrt(x)/src/LeetIndexTemplate/ProblemTest.java
@@ -24,14 +24,6 @@ class ProblemTest {
         assertEquals(expected, actual);
     }
 
-    @Test // maximum int size
-    public void intMax() {
-        int testInt = 1111111111;
-        int expected = 33333;
-        int actual = Problem.mySqrt(testInt);
-        assertEquals(expected, actual);
-    }
-
     @Test // casting long to collect maximum 2^31 input constraint
     public void longInt() {
         long testLong = 2147483647L;
@@ -40,4 +32,39 @@ class ProblemTest {
         int actual = Problem.mySqrt(longCast);
         assertEquals(expected, actual);
     }
+
+    @Test // confirming the rounding down of int cast
+    public void roundingDownIntCast() {
+        int testInt = 60; // == 7.75
+        int expected = 7; // should round down to 7
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
+
+            // typical cases
+
+    @Test // basic test included with leet example
+    public void basicLeetExample() {
+        int testInt = 4;
+        int expected = 2;
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
+
+    @Test // a more complex typical case
+    public void typicalExample() {
+        int testInt = 159201;
+        int expected = 399;
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
+
+    @Test // maximum int size
+    public void intMax() {
+        int testInt = 1111111111;
+        int expected = 33333;
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
+
 }
