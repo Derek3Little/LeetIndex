139e265209a728c4de48da4f55aba64dee17c3fc
Derek E Little <derek3little@gmail.com>
2023-02-04 09:02:28 -0500

    Adding sample input to Main and edge case tests to test suite.

--- Sqrt(x)/src/LeetIndexTemplate/Main.java
+++ Sqrt(x)/src/LeetIndexTemplate/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         // instantiate object of problem class
-
+        Problem problem = new Problem();
 
         // sample input
-
+        int testInt = 4;
 
         // print output to console
-
+        System.out.println(problem.mySqrt(testInt));
 
         // print confirmation program successfully finished to console
         System.out.println("Main program done!");
--- Sqrt(x)/src/LeetIndexTemplate/ProblemTest.java
+++ Sqrt(x)/src/LeetIndexTemplate/ProblemTest.java
@@ -1,7 +1,34 @@
 package LeetIndexTemplate;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProblemTest {
 
+        // edge cases
+
+    @Test // zero
+    public void zero() {
+        int testInt = 0;
+        int expected = 0;
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
+
+    @Test // one
+    public void one() {
+        int testInt = 1;
+        int expected = 1;
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
+
+    @Test // maximum int size
+    public void intMax() {
+        int testInt = 1111111111;
+        int expected = 33333;
+        int actual = Problem.mySqrt(testInt);
+        assertEquals(expected, actual);
+    }
 }
