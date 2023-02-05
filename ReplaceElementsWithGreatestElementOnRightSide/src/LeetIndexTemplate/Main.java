2693284d1a64e79090df4b83a2a6fefb595e783e
Derek E Little <derek3little@gmail.com>
2023-02-05 07:54:24 -0500

    Adding problem, main, and test classes.

---
+++ ReplaceElementsWithGreatestElementOnRightSide/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,20 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+
+
+        // print output to console
+        System.out.println();
+
+        // print confirmation program successfully finished to console
+        System.out.println("Main program done!");
+    }
+
+}
---
+++ ReplaceElementsWithGreatestElementOnRightSide/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,16 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem
+    // https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+
+    // SUBMITTED? yyyy.mm.dd?
+
+    public static int[] replaceElements(int[] arr) {
+
+        // temp dummy return
+        return arr;
+    }
+
+}
