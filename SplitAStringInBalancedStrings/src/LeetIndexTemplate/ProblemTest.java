bda6ffe6f4384de85f06249548a079eac14789d0
Derek E Little <derek3little@gmail.com>
2023-02-06 20:40:50 -0500

    Finishing SplitAStringInBalancedStrings test suite, all pass.

--- SplitAStringInBalancedStrings/src/LeetIndexTemplate/ProblemTest.java
+++ SplitAStringInBalancedStrings/src/LeetIndexTemplate/ProblemTest.java
@@ -11,9 +11,34 @@ class ProblemTest {
     @Test // minimum length string
     public void minimumLength() {
         String testString = "RL";
-        int actual = 1;
-        int expected = Problem.balancedStringSplit(testString);
+        int expected = 1;
+        int actual = Problem.balancedStringSplit(testString);
         assertEquals(expected, actual);
     }
 
+    @Test // single split
+    public void singleSplit() {
+        String testString = "RRRRRRRLLLLLLL";
+        int expected = 1;
+        int actual = Problem.balancedStringSplit(testString);
+        assertEquals(expected, actual);
+    }
+
+        // typical cases
+
+    @Test // basic test included with leet example
+    public void basicLeetExample() {
+        String testString = "RLRRLLRLRL";
+        int expected = 4;
+        int actual = Problem.balancedStringSplit(testString);
+        assertEquals(expected, actual);
+    }
+
+    @Test // a more complex case
+    public void typicalLargeString() {
+        String testString = "RRLLRLRRRLLLRLRRRRLLLLRLRL";
+        int expected = 7;
+        int actual = Problem.balancedStringSplit(testString);
+        assertEquals(expected, actual);
+    }
 }
