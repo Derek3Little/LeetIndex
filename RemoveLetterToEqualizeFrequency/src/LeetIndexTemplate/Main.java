ac5834147d5c8f3c43d2aa68d02916100e1ed4be
Derek E Little <derek3little@gmail.com>
2023-02-09 07:18:11 -0500

    Initial commit for RemoveLetterToEqualizeFrequency, adding starter project files.

---
+++ RemoveLetterToEqualizeFrequency/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,19 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+
+
+        // print output to console
+
+
+        // print confirmation program successfully finished to console
+        System.out.println("\nMain program done!");
+    }
+}
