8e68836c5068a9a7f70e0e6d1cfedd714441490f
Derek E Little <derek3little@gmail.com>
2023-02-09 08:31:25 -0500

    Finishing test suite, all pass.

--- RemoveLetterToEqualizeFrequency/src/LeetIndexTemplate/ProblemTest.java
+++ RemoveLetterToEqualizeFrequency/src/LeetIndexTemplate/ProblemTest.java
@@ -8,8 +8,43 @@ class ProblemTest {
 
     @Test // minimum input
     public void minimumInput() {
+        String testString = "aa"; // true
+        assertTrue(Problem.equalFrequency(testString));
+    }
+
+    @Test // almost minimum TRUE
+    public void almostMinimumInputT() {
         String testString = "aab"; // true
         assertTrue(Problem.equalFrequency(testString));
     }
 
+    @Test // almost minimum FALSE
+    public void almostMinimumInputF() {
+        String testString = "abc"; // false
+        assertTrue(!Problem.equalFrequency(testString)); // !
+    }
+
+    @Test // typical input TRUE
+    public void typicalInputT() {
+        String testString = "aabbccc";
+        assertTrue(Problem.equalFrequency(testString));
+    }
+
+    @Test // typical input FALSE
+    public void typicalInputF() {
+        String testString = "aabbccdd"; // false
+        assertTrue(!Problem.equalFrequency(testString)); // !
+    }
+
+    @Test // huge crazy input TRUE
+    public void largeInputT() {
+        String testString = "qwertyuiopqwertyuioppqwertyuiopqwertyuiop"; // note extra p in middle
+        assertTrue(Problem.equalFrequency(testString));
+    }
+
+    @Test // huge crazy input FALSE
+    public void largeInputF() {
+        String testString = "zxcvbnmzxcvbnmzxcvbnmzxcvbnmzxcvbnmzxcvbnmzxcvbnm"; // all equal == false
+        assertTrue(!Problem.equalFrequency(testString)); // !
+    }
 }
