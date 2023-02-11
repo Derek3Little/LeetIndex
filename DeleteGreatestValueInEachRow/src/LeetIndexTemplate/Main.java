de1c575fa49b12b15045fefb17cfbafe72708c6b
Derek E Little <derek3little@gmail.com>
2023-02-11 08:03:15 -0500

    Initial commit for DeleteGreatestValueInEachRow, adding project starter files and setting up problem.

---
+++ DeleteGreatestValueInEachRow/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,19 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+        int[][] testGrid = {{1,2,4},{3,3,1}};
+
+        // print output to console
+        System.out.println(problem.deleteGreatestValue(testGrid));
+
+        // print confirmation program successfully finished to console
+        System.out.println("\nMain program done!");
+    }
+}
---
+++ DeleteGreatestValueInEachRow/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,15 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/delete-greatest-value-in-each-row/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public static int deleteGreatestValue(int[][] grid) {
+
+        return 0;
+    }
+
+}
