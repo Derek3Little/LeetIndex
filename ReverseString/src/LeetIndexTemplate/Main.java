32710cfeae6b7d6d858698d2b7dd0ba5ba844d17
Derek E Little <derek3little@gmail.com>
2023-02-12 08:27:42 -0500

    Initial commit for ReverseString, adding starter project files and setting up.

---
+++ ReverseString/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,19 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+        char[] sampleString = {'h','i'};
+
+        // print output to console
+        System.out.println();
+
+        // print confirmation program successfully finished to console
+        System.out.println("\nMain program done!");
+    }
+}
---
+++ ReverseString/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,14 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/reverse-string/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public static void reverseString(char[] s) {
+
+    }
+
+}
