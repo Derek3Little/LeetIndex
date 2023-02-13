5f54b3c8c89f6bd383b8a4a7d2bac5029fc46af3
Derek E Little <derek3little@gmail.com>
2023-02-13 12:25:47 -0500

    Initial commit for SelfDividingNumbers, adding starter project files and setting up problem.

---
+++ SelfDividingNumbers/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,20 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+        int left = 1;
+        int right = 22;
+
+        // print output to console
+        problem.selfDividingNumbers(left, right);
+
+        // print confirmation program successfully finished to console
+        System.out.println("\nMain program done!");
+    }
+}
---
+++ SelfDividingNumbers/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,14 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/self-dividing-numbers/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+
+    }
+
+}
