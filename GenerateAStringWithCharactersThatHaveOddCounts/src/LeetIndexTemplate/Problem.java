82feb7d64ee5aca2eb40fc8004be2a1d34921463
Derek E Little <derek3little@gmail.com>
2023-02-14 06:47:04 -0500

    Initial commit for GenerateAStringWithCharactersThatHaveOddCounts, adding starter project files and setting up problem.

---
+++ GenerateAStringWithCharactersThatHaveOddCounts/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,15 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public String generateTheString(int n) {
+
+        return "";
+    }
+
+}
