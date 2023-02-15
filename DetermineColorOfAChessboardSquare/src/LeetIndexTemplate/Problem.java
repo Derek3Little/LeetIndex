a28f5bebce55fcbe7db2a6ac4e4b8b1f51f73ef3
Derek E Little <derek3little@gmail.com>
2023-02-15 06:46:34 -0500

    Initial commit for DetermineColorOfAChessboardSquare, adding starter project files and setting up problem.

---
+++ DetermineColorOfAChessboardSquare/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,14 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/determine-color-of-a-chessboard-square/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public boolean squareIsWhite(String coordinates) {
+        
+    }
+
+}
