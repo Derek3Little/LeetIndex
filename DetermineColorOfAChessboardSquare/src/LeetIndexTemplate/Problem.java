1fd4972c933c143d0b53f3864684b9fef303d8d6
Derek E Little <derek3little@gmail.com>
2023-02-15 06:57:44 -0500

    Adding solution to DetermineColorOfAChessboardSquare.

--- DetermineColorOfAChessboardSquare/src/LeetIndexTemplate/Problem.java
+++ DetermineColorOfAChessboardSquare/src/LeetIndexTemplate/Problem.java
@@ -8,7 +8,25 @@ public class Problem {
     // SUBMISSION DATE: yyyy.mm.dd
 
     public boolean squareIsWhite(String coordinates) {
-        
+
+        // default false on hypothetical 'a1'
+        boolean white = false;
+
+        // from default a1, toggle on 'odd' characters
+        switch (coordinates.charAt(0)) {
+            case 'b', 'd', 'f', 'h':
+                white = !white;
+                break;
+        }
+
+        // from default a1, toggle on even numbers
+        switch (coordinates.charAt(1)) {
+            case '2', '4', '6', '8':
+                white = !white;
+                break;
+        }
+
+        return white;
     }
 
 }
