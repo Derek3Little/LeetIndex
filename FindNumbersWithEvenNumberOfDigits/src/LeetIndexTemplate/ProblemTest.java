a8c58b9c8d573a3e9946757da40d97363e3c12a1
Derek E Little <derek3little@gmail.com>
2023-02-16 07:51:16 -0500

    Adding complete test suite to FindNumbersWithEvenNumberOfDigits, all pass.

--- FindNumbersWithEvenNumberOfDigits/src/LeetIndexTemplate/ProblemTest.java
+++ FindNumbersWithEvenNumberOfDigits/src/LeetIndexTemplate/ProblemTest.java
@@ -6,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProblemTest {
 
-    @Test
+        // findDigitCount tests
+
+    @Test // single digit
     public void findDigitCountOne()  {
         int testInt = 1;
         int expected = 1;
@@ -14,7 +16,7 @@ class ProblemTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @Test // two digits
     public void findDigitCountTwo()  {
         int testInt = 13;
         int expected = 2;
@@ -30,4 +32,45 @@ class ProblemTest {
         assertEquals(expected, actual);
     }
 
+        // findNumbers tests
+
+    @Test // single element array, zero evens
+    public void findNumbersSingleZero()  {
+        int[] testArray = {1};
+        int expected = 0;
+        int actual = Problem.findNumbers(testArray);
+        assertEquals(expected, actual);
+    }
+
+    @Test // single element array, zero evens
+    public void findNumbersSingleOne()  {
+        int[] testArray = {11};
+        int expected = 1;
+        int actual = Problem.findNumbers(testArray);
+        assertEquals(expected, actual);
+    }
+
+    @Test // large array, zero evens
+    public void findNumbersLargeInputZero()  {
+        int[] testArray = {1,123,345,56789,234,135,35764,90192};
+        int expected = 0;
+        int actual = Problem.findNumbers(testArray);
+        assertEquals(expected, actual);
+    }
+
+    @Test // large array, one even
+    public void findNumbersLargeInputOne()  {
+        int[] testArray = {1,123,345,56789,2134,135,35764,90192}; // 2134
+        int expected = 1;
+        int actual = Problem.findNumbers(testArray);
+        assertEquals(expected, actual);
+    }
+
+    @Test // large array, all evens
+    public void findNumbersLargeInputAll()  {
+        int[] testArray = {13,1233,3345,567389,2334,1335,353764,904192};
+        int expected = 8;
+        int actual = Problem.findNumbers(testArray);
+        assertEquals(expected, actual);
+    }
 }
