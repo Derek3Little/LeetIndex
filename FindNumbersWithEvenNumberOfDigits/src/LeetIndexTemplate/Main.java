da5f581fd810072fcf47cab56b20342c091f2764
Derek E Little <derek3little@gmail.com>
2023-02-16 07:37:41 -0500

    Initial commit for FindNumbersWithEvenNumberOfDigits, adding starter project files and setting up problem.

---
+++ FindNumbersWithEvenNumberOfDigits/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,19 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+        int[] sampleNums = {12,345,2,6,7896};
+
+        // print output to console
+        System.out.println(problem.findNumbers(sampleNums));
+
+        // print confirmation program successfully finished to console
+        System.out.println("\nMain program done!");
+    }
+}
---
+++ FindNumbersWithEvenNumberOfDigits/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,15 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public static int findNumbers(int[] nums) {
+
+        return 0;
+    }
+
+}
