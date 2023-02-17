ffe0078760ffab35541c3cc7a64377cd0fb582e9
Derek E Little <derek3little@gmail.com>
2023-02-17 07:31:44 -0500

    Finishing test suite, all pass.

--- LemonadeChange/src/LeetIndexTemplate/ProblemTest.java
+++ LemonadeChange/src/LeetIndexTemplate/ProblemTest.java
@@ -9,9 +9,42 @@ class ProblemTest {
     @Test
     public void singleFive() {
         int[] testBills = {5};
-        boolean expected = true;
-        boolean actual = Problem.lemonadeChange(testBills);
-        assertEquals(expected, actual);
+        assertTrue(Problem.lemonadeChange(testBills));
     }
 
+    @Test
+    public void singleTen() {
+        int[] testBills = {10};
+        assertFalse(Problem.lemonadeChange(testBills));
+    }
+
+    @Test
+    public void singleTwenty() {
+        int[] testBills = {20};
+        assertFalse(Problem.lemonadeChange(testBills));
+    }
+
+    @Test
+    public void leadingTen() {
+        int[] testBills = {10,5,5,5,5,5,20};
+        assertFalse(Problem.lemonadeChange(testBills));
+    }
+
+    @Test
+    public void leadingTwenty() {
+        int[] testBills = {20,5,5,5,5,5,10};
+        assertFalse(Problem.lemonadeChange(testBills));
+    }
+
+    @Test
+    public void endAtZeroTwenty() {
+        int[] testBills = {5,10,5,20};
+        assertTrue(Problem.lemonadeChange(testBills));
+    }
+
+    @Test
+    public void endAtNegativeTwenty() {
+        int[] testBills = {5,10,20};
+        assertFalse(Problem.lemonadeChange(testBills));
+    }
 }
