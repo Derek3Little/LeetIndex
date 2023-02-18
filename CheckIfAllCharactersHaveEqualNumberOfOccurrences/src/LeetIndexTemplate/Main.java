7d515cee31cbd8be72d196f3ec99a38b484b6daf
Derek E Little <derek3little@gmail.com>
2023-02-18 07:53:07 -0500

    Initial commit for CheckIfAllCharactersHaveEqualNumberOfOccurrences, adding starter project files and setting up problem.

---
+++ CheckIfAllCharactersHaveEqualNumberOfOccurrences/src/LeetIndexTemplate/Main.java
@@ -0,0 +1,19 @@
+package LeetIndexTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // instantiate object of problem class
+        Problem problem = new Problem();
+
+        // sample input
+        String sampleS = "abacbc";
+
+        // print output to console
+        problem.areOccurrencesEqual(sampleS);
+
+        // print confirmation program successfully finished to console
+        System.out.println("\nMain program done!");
+    }
+}
---
+++ CheckIfAllCharactersHaveEqualNumberOfOccurrences/src/LeetIndexTemplate/Problem.java
@@ -0,0 +1,14 @@
+package LeetIndexTemplate;
+
+public class Problem {
+
+    // url of problem:
+    // https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/
+
+    // SUBMISSION DATE: yyyy.mm.dd
+
+    public static boolean areOccurrencesEqual(String s) {
+
+    }
+
+}
